61db1c1d266b013fc0386ec8bc7c057c18da05ae
ruwzeta <ruwindhu123@gmail.com>
2019-05-31 06:14:16 +0530

    First commit

---
+++ app/src/main/java/lk/apiit/anthropometricmeasure/InputHeightActvity.java
@@ -0,0 +1,19 @@
+package lk.apiit.anthropometricmeasure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.widget.EditText;
+
+public class InputHeightActvity extends AppCompatActivity {
+
+
+     EditText incheet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_height_actvity);
+    }
+
+}
